6f233bfda39017321866f6bba38586bd4f5585a4
cyryll <cyrylpaull@gmail.com>
2021-02-25 11:27:54 +0300

    task-list

--- api/views.py
+++ api/views.py
@@ -3,7 +3,9 @@ from django.http import JsonResponse
 
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .serializer import TaskSerializer
 
+from .models import Task
 # Create your views here.
 
 @api_view(['GET'])
@@ -16,4 +18,10 @@ def apiOverview(request):
 		'Delete':'/task-delete/<str:pk>/',
 		}
 
-	return Response(api_urls)
+    return Response(api_urls)
+
+@api_view(['GET'])
+def taskList(request):
+	tasks = Task.objects.all()
+	serializer = TaskSerializer(tasks, many=True)
+	return Response(serializer.data)
